8069ec66dd2bb797292eb00fb6f8c6747725a1e1
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-11-27 19:50:49 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace MoldovanPaulaLab7
+﻿using System;
+using MoldovanPaulaLab7.Data;
+using System.IO;
+
+namespace MoldovanPaulaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
