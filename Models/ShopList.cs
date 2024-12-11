f4926265dade7cb9305ff49a882499381452b275
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-12-11 19:21:01 +0200

    lab10 partial

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace MoldovanPaulaLab7.Models
 {
@@ -14,5 +15,8 @@ namespace MoldovanPaulaLab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
     }
 }
---
+++ ShopEntryPage.xaml.cs
@@ -0,0 +1,38 @@
+using MoldovanPaulaLab7.Models;
+
+namespace MoldovanPaulaLab7;
+
+public partial class ShopEntryPage : ContentPage
+{
+	public ShopEntryPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        listView.ItemsSource = await App.Database.GetShopsAsync();
+    }
+
+    async void OnShopAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ShopPage
+        {
+            BindingContext = new Shop()
+        });
+    }
+
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ShopPage
+            {
+                BindingContext = e.SelectedItem as Shop
+            });
+        }
+
+    }
+}
