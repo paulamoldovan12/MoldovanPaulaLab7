034f94c26ab9efcda8e8af413908837c58af562a
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-12-11 20:12:43 +0200

    lab10 final

--- ShopPage.xaml.cs
+++ ShopPage.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.Maui.Devices.Sensors;
 using MoldovanPaulaLab7.Models;
+using Plugin.LocalNotification;
 
 namespace MoldovanPaulaLab7;
 
@@ -27,9 +29,32 @@ public partial class ShopPage : ContentPage
             Name = "Magazinul meu preferat" 
         };
 
-       // var shoplocation = locations?.FirstOrDefault();
-        var shoplocation= new Location(46.7492379, 23.5745597); //pentru Windows Machine 
-        await Map.OpenAsync(shoplocation, options);
+         var shoplocation = locations?.FirstOrDefault();
+        // var shoplocation= new Location(46.7492379, 23.5745597); //pentru Windows Machine 
+
+         var myLocation = await Geolocation.GetLocationAsync();
+         /*   var myLocation = new Location(46.7731796289, 23.6213886738); //pentru Windows Machine */
+
+         var distance = myLocation.CalculateDistance(shoplocation, DistanceUnits.Kilometers);
+
+         if (distance < 5)
+         {
+
+             var request = new NotificationRequest
+             {
+
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+
+             };
+             LocalNotificationCenter.Current.Show(request);
+         }
+
+         await Map.OpenAsync(shoplocation, options);
     }
 
 }
